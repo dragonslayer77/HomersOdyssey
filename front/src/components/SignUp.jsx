f36525325d1c8f59aa843ccdfdb1e368db002337
Isabella <isabellaafry@gmail.com>
2019-07-01 15:17:27 +0100

    not done with quest 060

--- front/src/components/SignUp.jsx
+++ front/src/components/SignUp.jsx
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Snackbar from '@material-ui/core/Snackbar';
+
 
 class SignUp extends Component {
   constructor(props){
@@ -12,49 +16,61 @@ class SignUp extends Component {
       flash: ''
     }
   }
-
+  
   updateField = (e) => {
     this.setState({[e.target.name]: e.target.value});
   }
 
-
+  flashColor = () =>{
+    this.state.flash === "User has been signed up!" ? this.setState({
+      backgroundColor: 'green'
+    }) : this.setState({
+      backgroundColor: 'red'
+    }) 
+  }
   handleSubmit = (e) => {
     fetch("/auth/signup",
     {
-          method:  'POST',
+      method:  'POST',
           headers:  new Headers({
-                'Content-Type':  'application/json'
+            'Content-Type':  'application/json'
           }),
           body:  JSON.stringify(this.state),
     })
     .then(res  =>  res.json())
     .then(
-          res  =>  this.setState({flash:  res.flash}),
-          err  =>  this.setState({flash:  err.flash})
-    )
-    console.log(`information submitted: ${JSON.stringify(this.state)}`);
-    e.preventDefault();
-  }
-
-  render(){
-    return(
-      <div>
+      res  =>  this.setState({flash:  res.flash}),
+      err  =>  this.setState({flash:  err.flash})
+      )
+      console.log(`information submitted: ${JSON.stringify(this.state)}`);
+      e.preventDefault();
+    }
+    
+    render(){
+      return(
+        <div>
         <div className={this.state.flash === "User has been signed up!" ? 'green': 'red'}>
-        <p>{this.state.flash}</p>
+          <Snackbar
+            open={this.state.submitted}
+            onClose={this.handleClose}
+            TransitionComponent={this.state.Transition}
+            ContentProps={{'aria-describedby': 'message-id'}}
+            message={<span id='message-id'>{this.state.flash}</span>}/>
+        {/* <p>{this.state.flash}</p> */}
         </div>
         <h1>Sign Up</h1>
         <form action="POST" onSubmit={this.handleSubmit}>
-          <input type="email" name="email" onChange={this.updateField} placeholder='example@example.com'/>
+          <TextField type="email" name="email" onChange={this.updateField} placeholder='example@example.com'/>
           <br/>
-          <input type="password" name="password" onChange={this.updateField} placeholder='password'/>
+          <TextField type="password" name="password" onChange={this.updateField} placeholder='password'/>
           <br/>
-          <input type="password" name="verificationPassword" onChange={this.updateField} placeholder='password'/>
+          <TextField type="password" name="verificationPassword" onChange={this.updateField} placeholder='password'/>
           <br/>
-          <input type="name" name="name" onChange={this.updateField} placeholder='John'/>
+          <TextField type="name" name="name" onChange={this.updateField} placeholder='John'/>
           <br/>
-          <input type="lastname" name="lastname" onChange={this.updateField} placeholder='Doe'/>
+          <TextField type="lastname" name="lastname" onChange={this.updateField} placeholder='Doe'/>
           <br/>
-          <input type="submit" value='Submit'/>
+          <Button color="secondary" type="submit" value='Submit'>Submit</Button>
 
         </form>
       </div>
