21e348a44ae5f4403d58b62d5cd745514c0972c6
Isabella <isabellaafry@gmail.com>
2019-07-08 10:09:51 +0100

    quest 060 completed

--- front/src/components/SignUp.jsx
+++ front/src/components/SignUp.jsx
@@ -13,7 +13,8 @@ class SignUp extends Component {
       verificationPassword: 'mypassword',
       name: 'jon',
       lastname: 'doe',
-      flash: ''
+      flash: '',
+      submitted: false
     }
   }
   
@@ -21,13 +22,13 @@ class SignUp extends Component {
     this.setState({[e.target.name]: e.target.value});
   }
 
-  flashColor = () =>{
-    this.state.flash === "User has been signed up!" ? this.setState({
-      backgroundColor: 'green'
-    }) : this.setState({
-      backgroundColor: 'red'
-    }) 
-  }
+  // flashColor = () =>{
+  //   this.state.flash === "User has been signed up!" ? this.setState({
+  //     backgroundColor: 'green'
+  //   }) : this.setState({
+  //     backgroundColor: 'red'
+  //   }) 
+  // }
   handleSubmit = (e) => {
     fetch("/auth/signup",
     {
@@ -39,38 +40,78 @@ class SignUp extends Component {
     })
     .then(res  =>  res.json())
     .then(
-      res  =>  this.setState({flash:  res.flash}),
-      err  =>  this.setState({flash:  err.flash})
+      res  =>  this.setState({
+        flash:  res.flash,
+        submitted: true
+      }),
+      err  =>  this.setState({
+        flash:  err.flash,
+        submitted: true
+      })
       )
       console.log(`information submitted: ${JSON.stringify(this.state)}`);
       e.preventDefault();
     }
+
+    handleClose = () => {
+      this.setState({submitted: false});
+    };
     
     render(){
       return(
         <div>
         <div className={this.state.flash === "User has been signed up!" ? 'green': 'red'}>
           <Snackbar
-            open={this.state.submitted}
-            onClose={this.handleClose}
-            TransitionComponent={this.state.Transition}
-            ContentProps={{'aria-describedby': 'message-id'}}
-            message={<span id='message-id'>{this.state.flash}</span>}/>
-        {/* <p>{this.state.flash}</p> */}
+              open={this.state.submitted}
+              onClose={this.handleClose}
+              // TransitionComponent={this.state.submitted}
+              ContentProps={{
+                'aria-describedby': 'message-id',
+              }}
+              message={<span id='message-id'>{this.state.flash}</span>}
+            />
         </div>
         <h1>Sign Up</h1>
         <form action="POST" onSubmit={this.handleSubmit}>
-          <TextField type="email" name="email" onChange={this.updateField} placeholder='example@example.com'/>
+          <TextField 
+            type="email" 
+            label="Email:"
+            name="email" 
+            onChange={this.updateField} 
+            placeholder='example@example.com'/>
           <br/>
-          <TextField type="password" name="password" onChange={this.updateField} placeholder='password'/>
+          <TextField 
+            type="password" 
+            label="Password:"
+            name="password" 
+            onChange={this.updateField} 
+            placeholder='password'/>
           <br/>
-          <TextField type="password" name="verificationPassword" onChange={this.updateField} placeholder='password'/>
+          <TextField 
+            type="password"
+            label="Password Copy:" 
+            name="verificationPassword" 
+            onChange={this.updateField} 
+            placeholder='password'/>
           <br/>
-          <TextField type="name" name="name" onChange={this.updateField} placeholder='John'/>
+          <TextField 
+            type="name" 
+            label="Firstname:"
+            name="name" 
+            onChange={this.updateField} 
+            placeholder='John'/>
           <br/>
-          <TextField type="lastname" name="lastname" onChange={this.updateField} placeholder='Doe'/>
+          <TextField 
+            type="lastname" 
+            label="Lastname:"
+            name="lastname" 
+            onChange={this.updateField} 
+            placeholder='Doe'/>
           <br/>
-          <Button color="secondary" type="submit" value='Submit'>Submit</Button>
+          <Button 
+            color="secondary" 
+            type="submit" 
+            value='Submit'>Submit</Button>
 
         </form>
       </div>
