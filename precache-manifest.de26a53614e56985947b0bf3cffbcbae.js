41b9741fd89e7df35c0bb4f08886710d29603b53
Isabella <isabellaafry@gmail.com>
2019-07-08 11:48:51 +0100

    Updates

---
+++ precache-manifest.de26a53614e56985947b0bf3cffbcbae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7a2761f2a46a3542b109b1464297b0",
+    "url": "/HomersOdyssey/index.html"
+  },
+  {
+    "revision": "9f026e08e7954b91e472",
+    "url": "/HomersOdyssey/static/css/main.948adf86.chunk.css"
+  },
+  {
+    "revision": "1c30c998c37af9af05d2",
+    "url": "/HomersOdyssey/static/js/2.dc8839ce.chunk.js"
+  },
+  {
+    "revision": "9f026e08e7954b91e472",
+    "url": "/HomersOdyssey/static/js/main.f3bc8bed.chunk.js"
+  },
+  {
+    "revision": "56154b165f7030479e19",
+    "url": "/HomersOdyssey/static/js/runtime~main.aa019dd8.js"
+  }
+]);
